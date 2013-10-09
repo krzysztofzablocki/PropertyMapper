9467c8f57e4f33d2d9c90b5bbb26ef0d2248265f
Krzysztof Zablocki <krzysztof.zablocki@me.com>
2013-10-09 11:36:22 +0100

    added ability to disable property getter call and thus verification

--- KZPropertyMapper/KZPropertyMapper.h
+++ KZPropertyMapper/KZPropertyMapper.h
@@ -12,14 +12,20 @@
 #define KZProperty(property) KZPropertyT(self, property)
 #define KZCall(method, property) KZCallT(self, method, property)
 
-#define KZBoxT(target, mapping, property) ({[target property], [KZPropertyDescriptor descriptorWithPropertyName:@#property andMapping:@#mapping];})
-#define KZPropertyT(target, property) ({[target property], [KZPropertyDescriptor descriptorWithPropertyName:@#property andMapping:nil];})
-#define KZCallT(target, method, property) ({[target property], [KZPropertyDescriptor descriptorWithPropertyName:@#property selector:@selector(method)];})
-
+#ifdef KZPropertyMapperDisableCompileTimeChecking
+  #define KZBoxT(target, mapping, property) ({[KZPropertyDescriptor descriptorWithPropertyName:@#property andMapping:@#mapping];})
+  #define KZPropertyT(target, property) ({[KZPropertyDescriptor descriptorWithPropertyName:@#property andMapping:nil];})
+  #define KZCallT(target, method, property) ({[KZPropertyDescriptor descriptorWithPropertyName:@#property selector:@selector(method)];})
+#else
+  #define KZBoxT(target, mapping, property) ({[target property], [KZPropertyDescriptor descriptorWithPropertyName:@#property andMapping:@#mapping];})
+  #define KZPropertyT(target, property) ({[target property], [KZPropertyDescriptor descriptorWithPropertyName:@#property andMapping:nil];})
+  #define KZCallT(target, method, property) ({[target property], [KZPropertyDescriptor descriptorWithPropertyName:@#property selector:@selector(method)];})
+#endif
 
 @interface KZPropertyMapper : NSObject
 + (BOOL)mapValuesFrom:(id)arrayOrDictionary toInstance:(id)instance usingMapping:(NSDictionary *)parameterMapping;
-+ (BOOL)mapValuesFrom:(id)arrayOrDictionary toInstance:(id)instance usingMapping:(NSDictionary *)parameterMapping errors:(__autoreleasing NSArray**)errors;
+
++ (BOOL)mapValuesFrom:(id)arrayOrDictionary toInstance:(id)instance usingMapping:(NSDictionary *)parameterMapping errors:(__autoreleasing NSArray **)errors;
 @end
 
 @interface KZPropertyMapper (Debug)
