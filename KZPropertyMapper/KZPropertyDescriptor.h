0bc131c2ef36b506ed34593432a88a8a3c16ff70
ilya n <ilya.nikokoshev@gmail.com>
2015-06-27 20:44:41 +0300

    Give readonly access to .propertyName of KZPropertyDescriptor

--- KZPropertyMapper/KZPropertyDescriptor.h
+++ KZPropertyMapper/KZPropertyDescriptor.h
@@ -9,6 +9,7 @@
 
 @interface KZPropertyDescriptor : NSObject
 @property(nonatomic, copy, readonly) NSString *stringMapping;
+@property(nonatomic, copy, readonly) NSString *propertyName;
 
 + (instancetype)descriptorWithPropertyName:(NSString*)name andMapping:(NSString *)mapping;
 + (instancetype)descriptorWithPropertyName:(NSString*)name andMappings:(id)descriptors, ... NS_REQUIRES_NIL_TERMINATION;
